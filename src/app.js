c51b9f73c7e27c34681a727153774c5db9c441e7
Facundo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2024-11-29 12:00:24 +0000

    Domain Generator Done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "a"];
+  let adj = ["great", "big", "wonderful", "scary"];
+  let noun = ["jogger", "racoon", "dragon", "dog"];
+  let ext = [".com", ".net", "es", ".org", ".uy"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let h = 0; h < ext.length; h++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + ext[h]);
+        }
+      }
+    }
+  }
 };
